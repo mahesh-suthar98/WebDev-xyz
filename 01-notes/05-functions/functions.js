eb84e521bad534b22394b7660f8efed5739adab2
MAHESH SUTHAR <kwodhios@gmail.com>
2025-12-22 17:26:54 +0530

    functions in js

---
+++ 01-notes/05-functions/functions.js
@@ -0,0 +1,16 @@
+function demo(p1=0, p2=0){
+    console.log(p1 + p2)
+}
+
+demo(3,3)
+
+console.log(demo)
+
+// alert("are u sure")
+
+console.log("paired")
+
+let x = 10
+let y = 5
+
+console.log((x+y)*2)
